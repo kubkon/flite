85caf592641185a2919ef844bd90cbb31fb3f534
Jakub Konka <jakub.konka@golem.network>
2020-01-13 17:26:29 +0100

    Simplify main

--- main/flite_main.c
+++ main/flite_main.c
@@ -40,7 +40,6 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <sys/time.h>
 #include <unistd.h>
 
 #include "flite.h"
@@ -93,18 +92,6 @@ static void flite_usage()
            "  --seti F=V  Set int feature\n"
            "  --setf F=V  Set float feature\n"
            "  --sets F=V  Set string feature\n"
-	   "  -ssml       Read input text/file in ssml mode\n"
-	   "  -b          Benchmark mode\n"
-	   "  -l          Loop endlessly\n"
-	   "  -voice NAME Use voice NAME (NAME can be pathname/url to flitevox file)\n"
-	   "  -voicedir NAME Directory containing (clunit) voice data\n"
-	   "  -lv         List voices available\n"
-	   "  -add_lex FILENAME add lex addenda from FILENAME\n"
-	   "  -pw         Print words\n"
-	   "  -ps         Print segments\n"
-           "  -psdur      Print segments and their durations (end-time)\n"
-	   "  -pr RelName Print relation RelName\n"
-           "  -voicedump FILENAME Dump selected (cg) voice to FILENAME\n"
            "  -v          Verbose mode\n");
     exit(0);
 }
@@ -197,7 +184,6 @@ static void ef_set(cst_features *f,const char *fv,const char *type)
 
 int main(int argc, char **argv)
 {
-    struct timeval tv;
     cst_voice *v;
     const char *filename;
     const char *outtype;
@@ -208,8 +194,6 @@ int main(int argc, char **argv)
     double time_start, time_end;
     int flite_verbose, flite_loop, flite_bench;
     int explicit_filename, explicit_text, explicit_phones, ssml_mode;
-#define ITER_MAX 3
-    int bench_iter = 0;
     cst_features *extra_feats;
     const char *lex_addenda_file = NULL;
     const char *voicedumpfile = NULL;
@@ -240,44 +224,6 @@ int main(int argc, char **argv)
 	    flite_usage();
 	else if (cst_streq(argv[i],"-v"))
 	    flite_verbose = TRUE;
-	else if (cst_streq(argv[i],"-lv"))
-        {
-            if (flite_voice_list == NULL)
-                flite_set_voice_list(voicedir);
-            flite_voice_list_print();
-            exit(0);
-        }
-	else if (cst_streq(argv[i],"-l"))
-	    flite_loop = TRUE;
-	else if (cst_streq(argv[i],"-b"))
-	{
-	    flite_bench = TRUE;
-	    break; /* ignore other arguments */
-	}
-	else if ((cst_streq(argv[i],"-o")) && (i+1 < argc))
-	{
-	    outtype = argv[i+1];
-	    i++;
-	}
-	else if ((cst_streq(argv[i],"-voice")) && (i+1 < argc))
-	{
-            if (flite_voice_list == NULL)
-                flite_set_voice_list(voicedir);
-            desired_voice = flite_voice_select(argv[i+1]);
-	    i++;
-	}
-	else if ((cst_streq(argv[i],"-voicedir")) && (i+1 < argc))
-	{
-            voicedir = argv[i+1];
-            if (flite_voice_list == NULL)
-                flite_set_voice_list(voicedir);
-	    i++;
-	}
-	else if ((cst_streq(argv[i],"-add_lex")) && (i+1 < argc))
-	{
-            lex_addenda_file = argv[i+1];
-	    i++;
-	}
 	else if (cst_streq(argv[i],"-f") && (i+1 < argc))
 	{
 	    filename = argv[i+1];
@@ -319,11 +265,6 @@ int main(int argc, char **argv)
 	    feat_set_string(extra_feats,"print_info_relation",argv[i+1]);
 	    feat_set(extra_feats,"post_synth_hook_func",
 		     uttfunc_val(&print_info));
-	    i++;
-	}
-	else if (cst_streq(argv[i],"-voicedump") && (i+1 < argc))
-	{
-            voicedumpfile = argv[i+1];
 	    i++;
 	}
 	else if ((cst_streq(argv[i],"-set") || cst_streq(argv[i],"-s"))
@@ -373,37 +314,9 @@ int main(int argc, char **argv)
 
     v = desired_voice;
     feat_copy_into(extra_feats,v->features);
-    durs = 0.0;
-
-    if (voicedumpfile != NULL)
-    {
-        flite_voice_dump(v,voicedumpfile);
-        exit(0);
-    }
-
-    if (lex_addenda_file)
-        flite_voice_add_lex_addenda(v,lex_addenda_file);
-
-    if (cst_streq("stream",outtype))
-    {
-        asi = new_audio_streaming_info();
-        asi->asc = audio_stream_chunk;
-        feat_set(v->features,"streaming_info",audio_streaming_info_val(asi));
-    }
-
-    if (flite_bench)
-    {
-	outtype = "none";
-	filename = "A whole joy was reaping, but they've gone south, you should fetch azure mike.";
-	explicit_text = TRUE;
-    }
-
-loop:
-    gettimeofday(&tv,NULL);
-    time_start = (double)(tv.tv_sec)+(((double)tv.tv_usec)/1000000.0);
 
     if (explicit_phones)
-	durs = flite_phones_to_speech(filename,v,outtype);
+	    flite_phones_to_speech(filename,v,outtype);
     else if ((strchr(filename,' ') && !explicit_filename) || explicit_text)
     {
         if (ssml_mode)
@@ -419,21 +332,8 @@ loop:
             durs = flite_file_to_speech(filename,v,outtype);
     }
 
-    gettimeofday(&tv,NULL);
-    time_end = ((double)(tv.tv_sec))+((double)tv.tv_usec/1000000.0);
-
-    if (flite_verbose || (flite_bench && bench_iter == ITER_MAX))
-	printf("times faster than real-time: %f\n(%f seconds of speech synthesized in %f)\n",
-	       durs/(float)(time_end-time_start),
-	       durs,
-	       (float)(time_end-time_start));
-
-    if (flite_loop || (flite_bench && bench_iter++ < ITER_MAX))
-	    goto loop;
-
     delete_features(extra_feats);
     delete_val(flite_voice_list); flite_voice_list=0;
-    /*    cst_alloc_debug_summary(); */
 
     return 0;
 }
